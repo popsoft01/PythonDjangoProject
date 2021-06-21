e04380c1a263b44db7509c5bd342a78d3c37be59
popsoft01 <popoolatunde52@gmail.com>
2021-06-21 08:42:07 +0100

    my task

---
+++ learning_logs/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+
+class Topic(models.Model):
+    """A topic the user is learning"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the model"""
+        return self.text
+
+
+# Create your models here.
+class Entry(models.Model):
+    """Something specific learned about a topic"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        if self.text.max_length > 50:
+            return self.text[:50] + "..."
